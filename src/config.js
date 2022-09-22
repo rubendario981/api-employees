a1197e7b75bfd69a66558d725269e687217fb89d
rubendario981 <rubendario981@hotmail.com>
2022-09-21 21:48:23 -0500

    Added the database port

--- src/config.js
+++ src/config.js
@@ -7,3 +7,4 @@ export const DATABASE = process.env.DATABASE
 export const HOST = process.env.HOST
 export const USER = process.env.USER
 export const PASSWORD = process.env.PASSWORD
+export const DB_PORT = process.env.DB_PORT
