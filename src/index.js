f3faa423d4cbb14366450ec95c491d600543766e
rubendario981 <rubendario981@hotmail.com>
2022-09-21 21:42:55 -0500

    loggin the name host

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import app from "./app.js"
 
 app.listen(PORT)
 
-<<<<<<< HEAD
+
 console.log("Server is running in the port ", PORT)
 console.log("Server is located in the host ", HOST)
-=======
-console.log("Server is running in the port ", PORT)
->>>>>>> 12c37f37b18fd9a7c8c3dd0012670283f3d907f0
