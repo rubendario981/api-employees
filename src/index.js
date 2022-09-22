c4609417675a3b5c9e70cf608f8d7c280975c819
rubendario981 <rubendario981@hotmail.com>
2022-09-21 20:54:21 -0500

    Script to start added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import express from 'express';
-import employeesRoutes from "./routes/employess.routes.js";
-import indexRoutes from "./routes/index.routes.js";
 import { PORT } from "./config.js"
-
-const app = express();
-
-app.use(express.json())
-
-app.use('/api',indexRoutes)
-app.use('/api',employeesRoutes)
-
-app.use((req, res, next)=>{
-    res.status(404).json({message: "Route doesn't exist"})
-})
+import app from "./app.js"
 
 app.listen(PORT)
 console.log("Server is running in port ", PORT)
