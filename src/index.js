5e44b52d1f76989d415b980151a9c3dcd57a877a
rubendario981 <rubendario981@hotmail.com>
2022-09-21 21:35:53 -0500

    testing minors changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { PORT } from "./config.js"
+import { HOST, PORT } from "./config.js"
 import app from "./app.js"
 
 app.listen(PORT)
-console.log("Server is running in port ", PORT)
+
+console.log("Server is running in the port ", PORT)
+console.log("Server is located in the host ", HOST)
